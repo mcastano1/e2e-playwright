987a7e814ee36c996f2a94ee605a01e31de97da3
mcastano1 <monicaj.castano@gmail.com>
2026-05-13 13:22:40 -0500

    first commit

---
+++ tests/example.spec.ts
@@ -0,0 +1,20 @@
+import { test, expect } from '@playwright/test';
+
+test('Buscar', async ({page})=>{
+  await page.goto('');
+  const input1 = await page.getByRole('combobox',{name:'Ingresa lo que quieras encontrar'})
+  await input1.fill('iphone');
+
+  await page.keyboard.press('Enter');
+  await expect(page.getByRole('heading',{ name:'Apple iPhone 17 (256 GB) - Lavanda - Distribuidor Autorizado' })).toBeVisible();
+
+  const products = await page.locator(".ui-search-layout__item").allInnerTexts()
+  console.log('the total of products is: ', products.length);
+
+  for (const product of products){
+     console.log('the title is: ', product);
+      
+  }
+
+
+});
